ca0485644c067081761528ca482d6172eb776b0c
Igor Ganapolsky <igor.ganapolsky@boats.com>
2025-02-14 16:14:00 -0500

    Improve nav bar design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,9 +9,11 @@ const NavBar = () => {
           BoatFinder
         </a>
         <div className="nav-buttons">
-          <button className="nav-button" aria-label="Search">
+          <button className="nav-button" aria-label="Help">
             <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M13.3333 13.3333L16.6667 16.6667M15 8.33333C15 11.555 12.555 14 9.33333 14C6.11167 14 3.66667 11.555 3.66667 8.33333C3.66667 5.11167 6.11167 2.66667 9.33333 2.66667C12.555 2.66667 15 5.11167 15 8.33333Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M10 18.3333C14.6024 18.3333 18.3334 14.6024 18.3334 10C18.3334 5.39763 14.6024 1.66667 10 1.66667C5.39765 1.66667 1.66669 5.39763 1.66669 10C1.66669 14.6024 5.39765 18.3333 10 18.3333Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M7.57501 7.5C7.77093 6.94306 8.15764 6.47342 8.66664 6.17428C9.17564 5.87513 9.77409 5.76578 10.3559 5.86559C10.9378 5.96541 11.4657 6.26794 11.8459 6.71961C12.2261 7.17128 12.4342 7.74294 12.4333 8.33334C12.4333 10 9.93335 10.8333 9.93335 10.8333" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M10 14.1667H10.0083" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
           </button>
           <button className="nav-button" aria-label="Account">
