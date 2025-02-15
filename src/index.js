f9edebe3bd1811fc1012b5e8f290f8054ffa5b89
Igor Ganapolsky <igor.ganapolsky@boats.com>
2025-02-15 14:16:59 -0500

    Modernize the code and remove unused files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 // Create a client
 const queryClient = new QueryClient({
@@ -24,8 +23,3 @@ root.render(
     </QueryClientProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
