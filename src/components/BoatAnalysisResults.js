bae1c6c873b9485f65e105e0fee217781b041026
Igor Ganapolsky <igor.ganapolsky@boats.com>
2025-02-17 15:16:24 -0500

    Fix layout of button

--- src/components/BoatAnalysisResults.js
+++ src/components/BoatAnalysisResults.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './BoatAnalysisResults.css';
 import SimilarBoats from './SimilarBoats';
+import { RefreshIcon } from './icons/RefreshIcon';
 
 const BoatAnalysisResults = ({ results, imagePreview, onReset }) => {
   const {
@@ -17,13 +18,11 @@ const BoatAnalysisResults = ({ results, imagePreview, onReset }) => {
         <div className="boat-image">
           <img src={imagePreview} alt="Analyzed boat" />
           <button onClick={onReset} className="new-search-button">
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M13.3333 8C13.3333 10.9467 10.9467 13.3333 8 13.3333C5.05333 13.3333 2.66667 10.9467 2.66667 8C2.66667 5.05333 5.05333 2.66667 8 2.66667M8 2.66667L10.6667 5.33333M8 2.66667L5.33333 5.33333" stroke="currentColor" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
+            <RefreshIcon />
             New Search
           </button>
         </div>
-
+        
         <div className="boat-details">
           <h2 className="uploaded-boat-title">Uploaded Boat</h2>
           
---
+++ src/components/icons/RefreshIcon.js
@@ -0,0 +1,10 @@
+export const RefreshIcon = () => (
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M13.3333 8C13.3333 10.9467 10.9467 13.3333 8 13.3333C5.05333 13.3333 2.66667 10.9467 2.66667 8C2.66667 5.05333 5.05333 2.66667 8 2.66667M8 2.66667L10.6667 5.33333M8 2.66667L5.33333 5.33333" 
+      stroke="currentColor" 
+      strokeWidth="1.33333" 
+      strokeLinecap="round" 
+      strokeLinejoin="round"
+    />
+  </svg>
+); 
